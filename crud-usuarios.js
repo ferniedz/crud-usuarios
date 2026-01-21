6978c886bda39cdacd83f6c0e6545997eb218258
Fernanda Niedzwiedki <ferniedzwiedki@outlook.com>
2026-01-21 15:19:48 -0300

    functions-editar-deletar-e-visualizar-usuario

--- crud-usuarios.js
+++ crud-usuarios.js
@@ -14,13 +14,137 @@ function perguntar(texto, callback) {
     });
 }
 
-function acharUsuarioPorId(id){
-    for(let i = 0; i < usuarios.length; i++){
-        if(usuarios[i].id === id){
+function acharUsuarioPorId(id) {
+    for (let i = 0; i < usuarios.length; i++) {
+        if (usuarios[i].id === id) {
             return i;
         }
     }
     return -1;
+}
+
+function cadastrarUsuario() {
+    perguntar("Insira o nome completo: ", (nomeCompletoStr) => {
+        perguntar("Insira o CPF: ", (cpfStr) => {
+            const nome = nomeCompletoStr.trim();
+            const cpf = Number(cpfStr);
+            if (!nome) {
+                console.log("\nERRO: Insira um nome válido.\n");
+                return menu();
+            }
+            if (Number.isNaN(cpf)) {
+                console.log("\nERRO: Insira um CPF válido.\n");
+                return menu();
+            }
+            const usuario = {
+                nome: nome,
+                cpf: cpf,
+                id: idUsuario
+            }
+            usuarios.push(usuario);
+            console.log("Usuário cadastrado com sucesso. ID do usuário: ", idUsuario);
+            idUsuario++;
+            return menu();
+        });
+    });
+}
+
+function listarUsuarios() {
+    if (usuarios.length === 0) {
+        console.log("\nERRO: Nenhum usuário encontrado.\n");
+        return menu();
+    } else {
+        for (let i = 0; i < usuarios.length; i++) {
+            console.log(
+                "Nome: ", usuarios[i].nome,
+                "| CPF: ", usuarios[i].cpf,
+                "| ID do usuário: ", usuarios[i].id,
+                "\n"
+            )
+        }
+        return menu();
+    }
+}
+
+function visualizarUsuario() {
+    perguntar("\nInsira o ID do usuário a ser visualizado: ", (idUsuarioStr) => {
+        const idUsuario = Number(idUsuarioStr);
+        const posicao = acharUsuarioPorId(idUsuario);
+        if (Number.isNaN(idUsuario) || idUsuario < 0) {
+            console.log("\nERRO: Insira um ID válido.");
+            return menu();
+        }
+        if (posicao === -1) {
+            console.log("\nERRO: Nenhum usuário com esse ID encontrado.\n");
+            return menu();
+        }
+        console.log(
+            "\n",
+            "Nome: ", usuarios[posicao].nome,
+            "| CPF: ", usuarios[posicao].cpf,
+            "\n"
+        );
+        return menu();
+    });
+}
+
+function editarUsuario() {
+    perguntar("\nInsira o ID do usuário a ser editado: ", (idUsuarioStr) => {
+        const idUsuario = Number(idUsuarioStr);
+        const posicao = acharUsuarioPorId(idUsuario);
+        if (Number.isNaN(idUsuario)) {
+            console.log("\nERRO: Insira um ID válido.");
+            return menu();
+        }
+        if (usuarios.length === 0) {
+            console.log("\nERRO: Nenhum usuário encontrado.\n");
+            return menu();
+        }
+        if (posicao === -1) {
+            console.log("\nERRO: Nenhum usuário com esse ID encontrado.\n");
+            return menu();
+        }
+        perguntar("\nInsira o novo nome completo:\n", (novoNomeStr) => {
+            perguntar("Insira o novo CPF: ", (novoCpfStr) => {
+                const novoNome = novoNomeStr.trim();
+                const novoCpf = Number(novoCpfStr);
+                if (!novoNome) {
+                    console.log("\nERRO: Insira um nome válido.\n");
+                    return menu();
+                }
+                if (Number.isNaN(novoCpf) || novoCpf < 0) {
+                    console.log("\nERRO: Insira um CPF válido.\n");
+                    return menu();
+                }
+                usuarios[posicao].nome = novoNome;
+                usuarios[posicao].cpf = novoCpf;
+                console.log("\nUsuário editado com sucesso.\n");
+                return menu();
+            });
+        });
+    });
+}
+
+function deletarUsuario() {
+    perguntar("\nInsira o ID do usuário a ser deletado:\n", (idUsuarioStr) => {
+        const idUsuario = Number(idUsuarioStr);
+        const posicao = acharUsuarioPorId(idUsuario);
+        if (Number.isNaN(idUsuario) || idUsuario < 0) {
+            console.log("\nERRO: Insira um ID válido.");
+            return menu();
+        }
+        if (usuarios.length === 0) {
+            console.log("\nERRO: Nenhum usuário encontrado.\n");
+            return menu();
+        }
+        if (posicao === -1) {
+            console.log("\nERRO: Nenhum usuário com esse ID encontrado.\n");
+            return menu();
+        }
+        usuarios.splice(posicao, 1);
+        console.log("\nUsuário deletado com sucesso.\n");
+        return menu();
+    });
 }
 
 function mostrarMenu() {
@@ -56,71 +180,6 @@ function menu() {
                 console.log("Opção inválida!");
                 return menu();
         }
-    });
-}
-
-function cadastrarUsuario() {
-    perguntar("Insira o nome completo: ", (nomeCompletoStr) => {
-        perguntar("Insira o CPF: ", (cpfStr) => {
-            const nome = nomeCompletoStr.trim();
-            const cpf = Number(cpfStr);
-            if (!nome) {
-                console.log("\nERRO: Insira um nome válido.\n");
-                return menu();
-            }
-            if (Number.isNaN(cpf)) {
-                console.log("\nERRO: Insira um CPF válido.\n");
-                return menu();
-            }
-            const usuario = {
-                nome: nome,
-                cpf: cpf,
-                id: idUsuario
-            }
-            usuarios.push(usuario);
-            console.log("Usuário cadastrado com sucesso. ID do usuário: ",idUsuario);
-            idUsuario++;
-            return menu();
-        });
-    });
-}
-
-function listarUsuarios() {
-    if (usuarios.length === 0) {
-        console.log("\nERRO: Nenhum usuário encontrado.\n");
-        return menu();
-    } else {
-        for (let i = 0; i < usuarios.length; i++) {
-            console.log(
-                "Nome: ", usuarios[i].nome,
-                "| CPF: ", usuarios[i].cpf,
-                "| ID do usuário: ", usuarios[i].id,
-                "\n"
-            )
-        }
-        return menu();
-    }
-}
-
-function deletarUsuario() {
-    perguntar("\nInsira o ID do usuário a ser deletado: ", (idDeletadoStr) => {
-        const idDeletado = Number(idDeletadoStr);
-        const posicao = acharUsuarioPorId(idDeletado);
-        if(Number.isNaN(idDeletado)){
-            console.log("\nERRO: Insira um ID válido.");
-            return menu();
-        }
-        if(usuarios.length === 0){
-            console.log("\nERRO: Nenhum usuário encontrado.\n");
-            return menu();
-        }
-        if(posicao === -1){
-            console.log("\nERRO: Nenhum usuário com esse ID encontrado.\n");
-            return menu();
-        }
-        usuarios.splice(posicao, 1);
-        console.log("\nUsuário deletado com sucesso.\n");
-        return menu();
     });
 }
 
